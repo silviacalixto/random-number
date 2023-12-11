29c35605d6b3504f5da084cd9c68c1d9299fa537
Sílvia Calixto <spatriciascl@gmail.com>
2023-12-11 17:22:06 -0300

    create random number console app

---
+++ Random_Number/Program.cs
@@ -0,0 +1,20 @@
+﻿var randNumber = new Random().Next(100);
+int numberEntered;
+
+
+do
+{
+    Console.WriteLine("Digite um número aleatório");
+    numberEntered = int.Parse(Console.ReadLine()!);
+
+    if (numberEntered > randNumber)
+        Console.WriteLine("O número digitado é maior do que o número randômico");
+
+    else if (randNumber > numberEntered)
+        Console.WriteLine("O número digitado é menor do que o número randômico");
+}
+
+while (numberEntered != randNumber);
+
+if (numberEntered == randNumber)
+    Console.WriteLine("O jogo terminou");
